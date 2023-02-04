9cd9df7ed7677aa26c67e0c77866046b92923ddb
p0ptartz <ericwm420@gmail.com>
2023-02-04 14:20:30 -0500

    added get * functionality -- added app.use json to parse json info

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const app = express()
 const PORT = 3200
 
 app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
 app.listen(PORT, () => {
