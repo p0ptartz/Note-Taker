19ff62871f2e6d27701aefba4e85e6ded644c364
p0ptartz <ericwm420@gmail.com>
2023-02-04 13:34:36 -0500

    made get route to  /notes in server.js file as a test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express()
 
 const PORT = 3200
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
 })
